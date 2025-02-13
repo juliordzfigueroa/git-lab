baea0332494c0f04d38840a9981f5905fd8237e6
Julio Rodriguez <jcrdzfigueroa@gmail.com>
2025-02-13 14:52:07 -0600

    Subidas ambas carpetas de las actividades de clase

---
+++ prueba_terminal/javascript_1.js
@@ -0,0 +1,2 @@
+//hola buenas no te olvides de suscribirte al canal y activar la campanita
+console.log("Ayuda")
---
+++ prueba_terminal2/javascript_2.js
@@ -0,0 +1,6 @@
+// Hola Profe, buena tarde, la clase de hoy me dejó las neuronas fritas pero estuvo god. Gracias por la clase
+function holaBuenas()
+{
+	console.log("Hola Buenas, no te olvides suscribir y activar la campanita")
+}
+
